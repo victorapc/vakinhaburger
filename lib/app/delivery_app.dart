713837d47acde8268d0e535497df296abbfa832f
victor <victor.alexandre@atlantasistemas.com>
2023-12-08 18:40:27 -0300

    Implementação dos componentes widgtes

--- lib/app/delivery_app.dart
+++ lib/app/delivery_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vakinhaburger/app/core/ui/theme/theme_config.dart';
 import 'package:vakinhaburger/app/pages/splash/splash_page.dart';
 
 class DeliveryApp extends StatelessWidget {
@@ -8,6 +9,7 @@ class DeliveryApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Delivery App',
+      theme: ThemeConfig.theme,
       routes: {
         '/': (context) => const SplashPage(),
       },
--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,44 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:vakinhaburger/app/core/ui/styles/app_styles.dart';
-import 'package:vakinhaburger/app/core/ui/styles/colors_app.dart';
+import 'package:vakinhaburger/app/core/ui/widgets/delivery_button.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Theme(
-      data: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          centerTitle: true,
-          iconTheme: IconThemeData(color: Colors.black),
-        ),
-        primaryColor: ColorsApp.i.primary,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: ColorsApp.i.primary,
-          primary: ColorsApp.i.primary,
-          secondary: ColorsApp.i.secondary,
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: AppStyles.i.primaryButton,
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Splash'),
       ),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Splash'),
-        ),
-        body: Column(
-          children: [
-            Container(),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Teste'),
-            ),
-          ],
-        ),
+      body: Column(
+        children: [
+          Container(),
+          DeliveryButton(
+            width: 200,
+            label: 'teste',
+            onPressed: () {},
+          ),
+          TextFormField(),
+        ],
       ),
     );
   }
