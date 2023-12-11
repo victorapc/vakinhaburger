f6894ae19a101ea30fab998549690298e50e8360
victor <victor.alexandre@atlantasistemas.com>
2023-12-11 17:58:21 -0300

    Configurações de variaveis, restclient e helpers

---
+++ lib/app/core/config/env/env.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Env {
+  static Env? _instance;
+
+  Env._();
+
+  static Env get i => _instance ??= Env._();
+
+  Future<void> load() => dotenv.load();
+
+  String? operator [](String key) => dotenv.env[key];
+}
