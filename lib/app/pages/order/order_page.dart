3c07a9b6b5fff9db6d811e916cf43d67b399712d
victor <victor.alexandre@atlantasistemas.com>
2024-01-08 16:27:08 -0300

    Tela order page view

--- lib/app/pages/order/order_page.dart
+++ lib/app/pages/order/order_page.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:vakinhaburger/app/core/ui/styles/text_styles.dart';
 import 'package:vakinhaburger/app/core/ui/widgets/delivery_appbar.dart';
+import 'package:vakinhaburger/app/core/ui/widgets/delivery_button.dart';
 import 'package:vakinhaburger/app/dto/order%20_product_dto.dart';
 import 'package:vakinhaburger/app/models/product_model.dart';
+import 'package:vakinhaburger/app/pages/order/widgets/order_field.dart';
 import 'package:vakinhaburger/app/pages/order/widgets/order_product_tile.dart';
+import 'package:vakinhaburger/app/pages/order/widgets/payment_types_field.dart';
+import 'package:validatorless/validatorless.dart';
 
 class OrderPage extends StatelessWidget {
   const OrderPage({super.key});
@@ -55,6 +59,72 @@ class OrderPage extends StatelessWidget {
                   ],
                 );
               },
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Total do pedido',
+                        style: context.textStyles.textExtraBold
+                            .copyWith(fontSize: 16),
+                      ),
+                      Text(
+                        r'R$ 20,00',
+                        style: context.textStyles.textExtraBold
+                            .copyWith(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                ),
+                const Divider(
+                  color: Colors.grey,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                OrderField(
+                  title: 'Endereço de Entrega',
+                  controller: TextEditingController(),
+                  validator: Validatorless.required('m'),
+                  hintText: 'Digite um endereço',
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                OrderField(
+                  title: 'CPF',
+                  controller: TextEditingController(),
+                  validator: Validatorless.required('m'),
+                  hintText: 'Digite o CPF',
+                ),
+                const PaymentTypesField(),
+              ],
+            ),
+          ),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Divider(
+                  color: Colors.grey,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: DeliveryButton(
+                    width: double.infinity,
+                    height: 48,
+                    label: 'FINALIZAR',
+                    onPressed: () {},
+                  ),
+                ),
+              ],
             ),
           ),
         ],
---
+++ lib/app/pages/order/widgets/order_field.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:vakinhaburger/app/core/ui/styles/text_styles.dart';
+
+class OrderField extends StatelessWidget {
+  final String title;
+  final TextEditingController controller;
+  final FormFieldValidator validator;
+  final String hintText;
+
+  const OrderField({
+    super.key,
+    required this.title,
+    required this.controller,
+    required this.validator,
+    required this.hintText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const defaultBorder =
+        UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey));
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 3.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 35,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Text(
+                title,
+                style: context.textStyles.textRegular.copyWith(
+                  overflow: TextOverflow.ellipsis,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              hintText: hintText,
+              border: defaultBorder,
+              enabledBorder: defaultBorder,
+              focusedErrorBorder: defaultBorder,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
