27a8eb3505e0a9f6be882fa178dfc8f94cdc827d
victor <victor.alexandre@atlantasistemas.com>
2024-01-22 15:33:01 -0300

    Funcionalidade refresh token

--- lib/app/core/restclient/custom_dio.dart
+++ lib/app/core/restclient/custom_dio.dart
@@ -17,7 +17,7 @@ class CustomDio extends DioForNative {
       requestHeader: true,
       responseHeader: true,
     ));
-    _authInterceptor = AuthInterceptor();
+    _authInterceptor = AuthInterceptor(this);
   }
 
   CustomDio auth() {
--- lib/app/core/restclient/interceptors/auth_interceptor.dart
+++ lib/app/core/restclient/interceptors/auth_interceptor.dart
@@ -1,8 +1,16 @@
+import 'dart:developer';
+
 import 'package:dio/dio.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:vakinhaburger/app/core/exceptions/expire_token_exception.dart';
 import 'package:vakinhaburger/app/core/global/global_context.dart';
+import 'package:vakinhaburger/app/core/restclient/custom_dio.dart';
 
 class AuthInterceptor extends Interceptor {
+  final CustomDio dio;
+
+  AuthInterceptor(this.dio);
+
   @override
   Future<void> onRequest(
       RequestOptions options, RequestInterceptorHandler handler) async {
@@ -17,9 +25,63 @@ class AuthInterceptor extends Interceptor {
   Future<void> onError(
       DioException err, ErrorInterceptorHandler handler) async {
     if (err.response?.statusCode == 401) {
-      GlobalContext.i.loginExpire();
+      try {
+        if (err.requestOptions.path != '/auth/refresh') {
+          await _refreshToken(err);
+          await _retryRequest(err, handler);
+        } else {
+          GlobalContext.i.loginExpire();
+        }
+      } catch (e) {
+        GlobalContext.i.loginExpire();
+      }
     } else {
       handler.next(err);
     }
+  }
+
+  Future<void> _refreshToken(DioException err) async {
+    try {
+      final sp = await SharedPreferences.getInstance();
+      final refreshToken = sp.getString('refreshToken');
+
+      if (refreshToken == null) {
+        return;
+      }
+
+      final resultRefresh = await dio.auth().put('/auth/refresh', data: {
+        'refresh_token': refreshToken,
+      });
+
+      sp.setString('accessToken', resultRefresh.data['access_token']);
+      sp.setString('refreshToken', resultRefresh.data['refresh_token']);
+    } on DioException catch (e, s) {
+      log('Erro ao realizar o refresh token.', error: e, stackTrace: s);
+      throw ExpireTokenException();
+    }
+  }
+
+  Future<void> _retryRequest(
+      DioException err, ErrorInterceptorHandler handler) async {
+    final requestOptions = err.requestOptions;
+
+    final result = await dio.request(
+      requestOptions.path,
+      options: Options(
+        headers: requestOptions.headers,
+        method: requestOptions.method,
+      ),
+      data: requestOptions.data,
+      queryParameters: requestOptions.queryParameters,
+    );
+
+    handler.resolve(
+      Response(
+        requestOptions: requestOptions,
+        data: result.data,
+        statusCode: result.statusCode,
+        statusMessage: result.statusMessage,
+      ),
+    );
   }
 }
